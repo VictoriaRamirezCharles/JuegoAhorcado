6c844b9a0d43e165dc8737ecd183d848c70897d3
Victoria Ramirez Charles <vramirez@pkmds.com>
2020-06-15 01:27:18 -0400

    Completado

---
+++ AhorcadoGame/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AhorcadoGame
+{
+   public class Animal
+    {
+        public string Nombre { get; set; }
+        public Animal(string Nombre)
+        {
+            this.Nombre = Nombre;
+        }
+    }
+}
---
+++ AhorcadoGame/Carro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AhorcadoGame
+{
+    public class Carro
+    {
+        public string Nombre { get; set; }
+
+        public Carro(string Nombre)
+        {
+            this.Nombre = Nombre;
+        }
+    }
+}
